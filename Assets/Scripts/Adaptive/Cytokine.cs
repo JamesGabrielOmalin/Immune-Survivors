7811894839fd250187eb6e298283108bd4e2ebd4
melon_pain <james_omalin@dlsu.edu.ph>
2023-09-30 23:35:55 +0800

    Adjusted cytokine and antibody appearance

--- Assets/Scripts/Adaptive/Cytokine.cs
+++ Assets/Scripts/Adaptive/Cytokine.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Cytokine : Projectile, IBodyColliderListener
 {
     [HideInInspector] public AntigenType Type { get; private set; }
     [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private VisualEffect vfx;
     [SerializeField] private List<Color> colors = new();
 
     public void OnBodyColliderEnter(Collider other)
     {
-        if (other.TryGetComponent<Player>(out Player player))
+        if (other.TryGetComponent(out Player player))
         {
             AttributeModifier mod = new(AntigenManager.instance.GetAntigenCount(Type) * 0.01f, AttributeModifierType.Multiply);
             player.ApplyAntigenBuffs(Type, mod, 2.5f);
@@ -28,6 +30,9 @@ public class Cytokine : Projectile, IBodyColliderListener
     {
         Type = newType;
 
-        sprite.color = colors[(int)newType];
+        var color = colors[(int)newType];
+
+        sprite.color = color;
+        vfx.SetVector4("Color", color);
     }
-}
+} 
