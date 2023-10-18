49df2098975953455c15ecdd5947c415a2db1d00
EM-Neon <blindsuneclipse@gmail.com>
2023-10-18 23:47:49 +0800

    Fixed the level select not updating, added new font (still have to change the old font to new font)

--- Assets/Scripts/Scene/MapSelect.cs
+++ Assets/Scripts/Scene/MapSelect.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,9 @@ public class MapSelect : MonoBehaviour
 {
     [SerializeField] private Button nextButton;
     [SerializeField] private Button previousButton;
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private Image mapPreview;
+    [SerializeField] private Sprite[] previewSprites;
 
     private int selectedLevel = 1;
 
@@ -18,6 +22,9 @@ public class MapSelect : MonoBehaviour
 
         nextButton.interactable = selectedLevel < MAX_LEVEL;
         previousButton.interactable = true;
+
+        UpdateText();
+        UpdatePreview();
     }
 
     public void PreviousLevel()
@@ -26,10 +33,23 @@ public class MapSelect : MonoBehaviour
 
         nextButton.interactable = true;
         previousButton.interactable = selectedLevel > 1;
+
+        UpdateText();
+        UpdatePreview();
     }
 
     public void LoadLevel()
     {
         SceneLoader.instance.LoadScene(selectedLevel);
+    }
+
+    public void UpdateText()
+    {
+        text.text = "Level " + selectedLevel;
+    }
+
+    public void UpdatePreview()
+    {
+        mapPreview.sprite = previewSprites[selectedLevel-1];
     }
 }
