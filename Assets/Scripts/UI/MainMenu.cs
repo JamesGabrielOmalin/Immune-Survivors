ae4c4acfc4925d7f008de992d4a058c5a32444ad
Set Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2023-10-28 21:20:20 +0800

    refactored my brute forced solution

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,27 +4,8 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    private SceneLoader sceneLoader;
-    // Start is called before the first frame update
-    void Start()
-    {
-        sceneLoader = GameObject.FindWithTag("SceneLoader").GetComponent<SceneLoader>();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ExitGame()
     {
-        if (sceneLoader == null)
-        {
-            Debug.Log(" Scene Loader NOT Found");
-            return;
-        }
-        sceneLoader.ExitGame();
+        SceneLoader.instance.ExitGame();
     }
 }
